9b84232f419030c17e1e04bf136260aae44db65e
joaoboy <joaodelacruzboy@gmail.com>
2022-09-04 22:26:07 -0300

    segundo commit

---
+++ example01.js
@@ -0,0 +1,6 @@
+"use strict";
+let a;
+let b;
+let c = 101;
+c = "one";
+let recursos = ['memoria', 'discos', 'procesador', '10'];
---
+++ example01.ts
@@ -0,0 +1,6 @@
+let a: number;
+let b: string;
+let c: (string|number) = 101;
+c="one";
+
+let recursos: string[] = ['memoria','discos','procesador', '10']
